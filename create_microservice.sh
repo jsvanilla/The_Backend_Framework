2b161bf9f92f61a8f3eb8017a026e3d570fecc92
jsvanilla <36719905+jsvanilla@users.noreply.github.com>
2023-04-09 15:58:35 -0600

    First Commit

---
+++ create_microservice.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# options to display
+options=("FastApi (Python)" "NodeJS (Javascript)" "Cancel\n")
+
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+PROMT_MESSAGE='echo -e "\033[0mCHOOSE THE BACKEND ENVIRONMENT: "\n'
+
+printf "\033[?25l\033[s"
+
+# Prompt the user for a microservice name
+echo -n "Enter a name for your microservice: "
+read microservice_name
+
+selected=0
+
+# Display options
+while true; do
+    printf "\033[u\n"
+    $PROMT_MESSAGE
+    for i in "${!options[@]}"; do
+        if [ $i -eq $selected ]; then
+            printf "${GREEN}➤ ${options[$i]}${NC}\n"
+        else
+            printf "  ${options[$i]}\n"
+        fi
+    done
+
+    # read user input
+    read -s -n1 input
+
+    # uptade selected option
+    case $input in
+        A) # Up Arrow
+            if [ $selected -gt 0 ]; then
+                selected=$((selected - 1))
+            fi
+            ;;
+        B) # Down Arrow
+            if [ $selected -lt $((${#options[@]} - 1)) ]; then
+                selected=$((selected + 1))
+            fi
+            ;;
+        "") # Enter
+            option="${options[$selected]}"
+            break
+            ;;
+        *) ;;
+    esac
+done
+
+printf "\033[u\033[?25h"
+
+clear
+
+# Execute selected option
+case $option in
+    "${options[0]}")
+        echo "Creating FastApi (Python) microservice $microservice_name ..."
+        # Python Commands
+        cd api/services && 
+        mkdir $microservice_name &&
+        cd $microservice_name
+        python3 -m venv venv &&
+        source venv/bin/activate &&
+        pip install --upgrade pip &&
+        pip install fastapi uvicorn httpx pytest
+        pip freeze > requirements.txt
+        file_content=$(cat ../../../templates/python/fastapi/basic_server.py)
+        test_content=$(cat ../../../templates/python/fastapi/test_basic_server.py)
+        new_content=${file_content//microservice_name/$($new_title)}
+        new_test_content=${test_content//microservice_name/$($new_title)}
+        echo "$new_content" > main.py
+        echo "$new_test_content" > test_main.py
+        ;;
+    "${options[1]}")
+        echo "Creating NodeJS (Javascript) microservice $microservice_name ..."
+        # Javascript Commands
+        ;;
+    "${options[2]}")
+        echo "Exiting..."
+        ;;
+    *) echo "Invalid Option";;
+esac
